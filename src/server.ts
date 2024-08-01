4a3fdfb3cd896ec9843b01111ed4c1e58354f8ed
= <yashbansodgit@gmail.com>
2024-08-01 22:34:07 +0530

    refactor: cleanup

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./common/middlewares/error-handler";
-import { categoryRouter } from "./category/router";
-import { productRouter } from "./product/router";
 
 export const createServer = (): Express => {
     const app = express();
@@ -21,8 +19,6 @@ export const createServer = (): Express => {
         .get("/message/:name", (req, res) => {
             return res.json({ message: `hello ${req.params.name}` });
         })
-        .use("/api/v1/category", categoryRouter)
-        .use("/api/v1/product", productRouter)
         .use(errorHandler);
 
     return app;
